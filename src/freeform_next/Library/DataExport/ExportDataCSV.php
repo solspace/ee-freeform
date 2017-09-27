bf424f2593e44a5bef22ae045d609e4208f4257e
Gustavs Gūtmanis <gustavs.gutmanis@gmail.com>
2017-09-27 14:02:56 +0300

    #145 fixing the CSV array export thing that was fixed in FFc a long time ago

--- src/freeform_next/Library/DataExport/ExportDataCSV.php
+++ src/freeform_next/Library/DataExport/ExportDataCSV.php
@@ -14,19 +14,25 @@ namespace Solspace\Addons\FreeformNext\Library\DataExport;
 /**
  * ExportDataCSV - Exports to CSV (comma separated value) format.
  */
-class ExportDataCSV extends ExportData {
-
-    function generateRow($row) {
+class ExportDataCSV extends ExportData
+{
+    /**
+     * @param array $row
+     *
+     * @return string
+     */
+    public function generateRow($row) {
         foreach ($row as $key => $value) {
-            // Escape inner quotes and wrap all contents in new quotes.
-            // Note that we are using \" to escape double quote not ""
-            $row[$key] = '"'. str_replace('"', '\"', $value) .'"';
+            // Escape inner quotes by double-quoting and wrap non-empty contents in new quotes
+            if ($value !== '') {
+                $row[$key] = '"' . str_replace('"', '""', $value) . '"';
+            }
         }
-        return implode(",", $row) . "\n";
+        return implode(',', $row) . "\n";
     }
 
-    function sendHttpHeaders() {
-        header("Content-type: text/csv");
-        header("Content-Disposition: attachment; filename=".basename($this->filename));
+    public function sendHttpHeaders() {
+        header('Content-type: text/csv');
+        header('Content-Disposition: attachment; filename=' .basename($this->filename));
     }
 }
