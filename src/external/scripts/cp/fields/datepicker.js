fc47ecb998cf1390d0c7c2ad2961b8cb1062165d
Gustavs Gūtmanis <gustavs.gutmanis@gmail.com>
2017-11-08 16:23:19 +0200

    fixing datepicker for IE11

--- src/external/scripts/cp/fields/datepicker.js
+++ src/external/scripts/cp/fields/datepicker.js
@@ -1,11 +1,34 @@
+if (typeof Object.assign != 'function') {
+  Object.assign = function(target) {
+    'use strict';
+    if (target == null) {
+      throw new TypeError('Cannot convert undefined or null to object');
+    }
+
+    target = Object(target);
+    for (var index = 1; index < arguments.length; index++) {
+      var source = arguments[index];
+      if (source != null) {
+        for (var key in source) {
+          if (Object.prototype.hasOwnProperty.call(source, key)) {
+            target[key] = source[key];
+          }
+        }
+      }
+    }
+    return target;
+  };
+}
+
 const datepickers = document.querySelectorAll('.form-datepicker');
 
 for (let i = 0; i < datepickers.length; i++) {
-  const picker = datepickers[i],
-      dateFormat = picker.dataset.datepickerFormat,
-      enableTime = picker.dataset.datepickerEnabletime,
-      enableDate = picker.dataset.datepickerEnabledate,
-      clock_24h = picker.dataset.datepickerClock_24h;
+  const picker = datepickers[i];
+
+  const dateFormat = picker.getAttribute("data-datepicker-format"),
+        enableTime   = picker.getAttribute("data-datepicker-enabletime"),
+        enableDate   = picker.getAttribute("data-datepicker-enabledate"),
+        clock_24h    = picker.getAttribute("data-datepicker-clock_24h");
 
   flatpickr(picker, {
     disableMobile: true,
