ef471732bacc0736fc265dd31712516249c7f39d
Gustavs Gūtmanis <gustavs.gutmanis@gmail.com>
2017-11-10 16:31:24 +0200

    making ffn EE3 backwards compatible lol

--- src/freeform_next/View/settings/common.php
+++ src/freeform_next/View/settings/common.php
@@ -1,3 +1,3 @@
-<div class="">
+<div class="<?php echo version_compare(APP_VER, '4.0.0', '<') ? 'box' : '' ?>">
     <?php $this->embed('ee:_shared/form')?>
 </div>
--- src/freeform_next/View/statuses/edit.php
+++ src/freeform_next/View/statuses/edit.php
@@ -1,4 +1,4 @@
-<div class="">
+<div class="<?php echo version_compare(APP_VER, '4.0.0', '<') ? 'box' : '' ?>">
     <?php $this->embed('ee:_shared/form') ?>
 </div>
 
