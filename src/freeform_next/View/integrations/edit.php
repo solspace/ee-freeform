7c77ba99e333026890c35d8c9a341398e2430fa1
Gustavs Gūtmanis <gustavs.gutmanis@gmail.com>
2017-11-10 16:47:59 +0200

    another same fix

--- src/freeform_next/View/integrations/edit.php
+++ src/freeform_next/View/integrations/edit.php
@@ -1,4 +1,4 @@
-<div class="<?php echo version_compare(APP_VER, '4.0.0', '<') ? 'box' : '' ?>">
+<div class="<?php echo version_compare(preg_replace('/[^0-9\.]/', '', APP_VER), '4.0.0', '<=') ? 'box' : '' ?>">
     <?php $this->embed('ee:_shared/form')?>
 </div>
 
