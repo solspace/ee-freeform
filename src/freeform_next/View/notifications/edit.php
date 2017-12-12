ff467d0419a01d3aa50a04997cd49d26f5de1393
Gustavs Gūtmanis <gustavs.gutmanis@gmail.com>
2017-12-12 15:08:58 +0200

    Fixing style issues with BOX classes

--- src/freeform_next/View/notifications/edit.php
+++ src/freeform_next/View/notifications/edit.php
@@ -1,3 +1,3 @@
-<div class="<?php echo version_compare(preg_replace('/[^0-9\.]/', '', APP_VER), '4.0.0', '<=') ? 'box' : '' ?>">
+<div class="<?php echo version_compare(APP_VER, '4.0.0', '<') ? 'box' : '' ?>">
     <?php $this->embed('ee:_shared/form')?>
 </div>
--- src/freeform_next/View/submissions/edit.php
+++ src/freeform_next/View/submissions/edit.php
@@ -1,5 +1,5 @@
 <link rel="stylesheet" href="<?php echo URL_THIRD_THEMES ?>freeform_next/css/builder.css" />
 
-<div class="<?php echo version_compare(preg_replace('/[^0-9\.]/', '', APP_VER), '4.0.0', '<=') ? 'box' : '' ?>">
+<div class="<?php echo version_compare(APP_VER, '4.0.0', '<') ? 'box' : '' ?>">
     <?php $this->embed('ee:_shared/form')?>
 </div>
