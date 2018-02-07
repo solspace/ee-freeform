27c60e2f89cb761cc8a9dda8e578e96973c1087c
Gustavs Gūtmanis <gustavs.gutmanis@gmail.com>
2018-02-07 16:49:27 +0200

    Attempting to make migration look better

--- src/freeform_next/View/migrations/re-install.php
+++ src/freeform_next/View/migrations/re-install.php
@@ -8,15 +8,24 @@
  * @link          https://solspace.com/expressionengine/freeform
  * @license       https://solspace.com/software/license-agreement
  */
-
-$this->extend('_layouts/table_form_wrapper')?>
+?>
 <link rel="stylesheet" href="<?php echo URL_THIRD_THEMES ?>freeform_next/css/migrations.css"/>
 
-<div id="reinstall-wrapper">
-    <div id="reinstall-title"><i class="fa fa-ban" aria-hidden="true"></i>Cannot Perform Migration</div>
-    <div id="reinstall-text">
-        Freeform Classic migration cannot be performed. The new Freeform version must be a fresh install.
-        To resolve this issue please uninstall the new Freeform version and install it again. Then attempt to run the
-        Migration utility again without modifying Freeform. If this error persists, please contact Solspace support.
+<div class="form-standard">
+    <div class="form-btns form-btns-top">
+        <h1><?= $cp_page_title ?></h1>
+
+    </div>
+
+
+    <div id="reinstall-wrapper" class="migration-info-block" style="margin: 10px 0 30px;">
+        <div id="reinstall-title"><i class="fa fa-ban" aria-hidden="true"></i>Cannot Perform Migration</div>
+        <div id="reinstall-text">
+            Freeform Classic migration cannot be performed. The new Freeform version must be a fresh install.
+            To resolve this issue please uninstall the new Freeform version and install it again. Then attempt to
+            run the
+            Migration utility again without modifying Freeform. If this error persists, please contact Solspace
+            support.
+        </div>
     </div>
 </div>
