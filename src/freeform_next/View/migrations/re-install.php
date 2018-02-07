c2326c15fdb64db5935660a9442af9cf1f638894
Kelsey Martens <kelsey@solspace.com>
2018-02-07 13:13:11 -0600

    Slapping ‘beta’ all over this migration

--- src/freeform_next/View/migrations/re-install.php
+++ src/freeform_next/View/migrations/re-install.php
@@ -13,7 +13,7 @@
 
 <div class="form-standard <?php echo version_compare(APP_VER, '4.0.0', '<') ? 'box' : '' ?>">
     <div class="form-btns form-btns-top">
-        <h1><?= $cp_page_title ?></h1>
+        <h1><?= $cp_page_title ?> (beta)</h1>
     </div>
 
     <div id="reinstall-wrapper" class="migration-info-block" style="margin: 10px 0 30px;">
