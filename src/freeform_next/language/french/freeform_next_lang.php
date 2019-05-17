111661d432257f8c992141bc38b707fa666fba97
Kelsey Martens <kelsey@solspace.com>
2019-05-16 23:38:32 -0500

    Adding basic Portuguese and French translations

---
+++ src/freeform_next/language/french/freeform_next_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang = [
+    'label' => 'Libellé',
+    'handle' => 'Identificateur' /* or 'titre' ??*/,
+    'is_default' => 'Est-ce l\'option par défaut?',
+    'blocked_spam_count' => 'Spam bloqué',
+    'no_available_composer_forms' => 'Aucun formulaire Composer disponible',
+    'today' => 'Aujourd\'hui',
+    'entry_date' => 'Date d\'entrée',
+    'this_week' => 'Cette semaine',
+    'this_month' => 'Ce mois',
+    'last_month' => 'Ce dernier mois',
+    'this_year' => 'Cette année',
+    'choose_date_range' => 'Choisissez un intervalle de dates',
+    'all_fields' => 'Tous les champs',
+    'field' => 'Champs',
+    'date_range' => 'Intervalle de dates',
+    'start_date' => 'Date de début',
+    'end_date' => 'Date de fin',
+    'keywords' => 'Mots clés',
+	'{email} is not a valid email address' => '{email} n\'est pas une adresse valide',
+	'This field is required' => 'Ce champ est requis',
+];
