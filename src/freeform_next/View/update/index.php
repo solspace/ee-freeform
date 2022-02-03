e327cb42b77d0d4257bdc717b95bdf56bd6f7b64
nicolasbottari <ydHad60gwjdoND7sLB/h2HyUKzgEy"y2>
2022-02-03 12:28:13 +0900

    Updated Freeform's `/updates` section display

--- src/freeform_next/View/update/index.php
+++ src/freeform_next/View/update/index.php
@@ -6,52 +6,57 @@
 <link rel="stylesheet" href="<?php echo URL_THIRD_THEMES ?>freeform_next/css/updates.css"/>
 
 
-<div class="box table-list-wrap">
-    <div class="tbl-ctrls">
+<div class="panel">
+	<div class="panel-heading">
+		<div class="title-bar title-bar--large">
+			<h3 class="title-bar__title">
+				Updates Available (<?php echo count($updates) ?>)
+			</h3>
 
-        <fieldset class="tbl-search right">
-            <a class="btn tn action" target="_blank" href="https://expressionengine.com/store/purchases">
-                Get Latest Version
-            </a>
-        </fieldset>
+			<div class="title-bar__extra-tools">
+				<a class="button button--primary" target="_blank" href="https://expressionengine.com/store/purchases">
+					Get Latest Version
+				</a>
+			</div>
+		</div>
+	</div>
 
-        <h1 style="margin-bottom: 10px;">
-            Updates Available (<?php echo count($updates) ?>)
-        </h1>
+	<div class="panel-body">
 
-        <?php foreach ($updates as $item) : ?>
 
-            <section class="item-wrap">
-                <div class="item">
-                    <h3>
-                        Freeform <b><?= $item->getVersion() ?></b>
-                        <i>
-                            (released on
-                            <?= ee()->localize->format_date($format, $item->getDate()->getTimestamp()) ?>)
-                        </i>
-                    </h3>
-                    <div class="message">
-                        <ul class="update-list">
-                            <?php foreach ($item->getFeatures() as $note) : ?>
-                                <li class="feature">
-                                    <?php echo $note ?>
-                                </li>
-                            <?php endforeach; ?>
-                            <?php foreach ($item->getNotes() as $note) : ?>
-                                <li class="note">
-                                    <?php echo $note ?>
-                                </li>
-                            <?php endforeach; ?>
-                            <?php foreach ($item->getBugfixes() as $note) : ?>
-                                <li class="bugfix">
-                                    <?php echo $note ?>
-                                </li>
-                            <?php endforeach; ?>
-                        </ul>
-                    </div>
-                </div>
-            </section>
+		<?php foreach ($updates as $item) : ?>
 
-        <?php endforeach; ?>
-    </div>
+			<section class="item-wrap">
+				<div class="item">
+					<h3>
+						Freeform <b><?= $item->getVersion() ?></b>
+						<i>
+							(released on
+							<?= ee()->localize->format_date($format, $item->getDate()->getTimestamp()) ?>)
+						</i>
+					</h3>
+					<div class="message">
+						<ul class="update-list">
+							<?php foreach ($item->getFeatures() as $note) : ?>
+								<li class="feature">
+									<?php echo $note ?>
+								</li>
+							<?php endforeach; ?>
+							<?php foreach ($item->getNotes() as $note) : ?>
+								<li class="note">
+									<?php echo $note ?>
+								</li>
+							<?php endforeach; ?>
+							<?php foreach ($item->getBugfixes() as $note) : ?>
+								<li class="bugfix">
+									<?php echo $note ?>
+								</li>
+							<?php endforeach; ?>
+						</ul>
+					</div>
+				</div>
+			</section>
+
+		<?php endforeach; ?>
+	</div>
 </div>
