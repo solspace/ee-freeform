c6dfe66f32f8c0158a6fd8d803f6576e2e5879a9
kjmartens <kelsey@solspace.com>
2022-10-27 13:41:57 -0500

    3.0.0-b.3

--- src/freeform_next/Services/AbstractIntegrationService.php
+++ src/freeform_next/Services/AbstractIntegrationService.php
@@ -1,9 +1,12 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: gustavs
- * Date: 30/08/2017
- * Time: 17:29.
+ * Freeform for ExpressionEngine
+ *
+ * @package       Solspace:Freeform
+ * @author        Solspace, Inc.
+ * @copyright     Copyright (c) 2008-2022, Solspace, Inc.
+ * @link          https://docs.solspace.com/expressionengine/freeform/v3/
+ * @license       https://docs.solspace.com/license-agreement/
  */
 
 namespace Solspace\Addons\FreeformNext\Services;
