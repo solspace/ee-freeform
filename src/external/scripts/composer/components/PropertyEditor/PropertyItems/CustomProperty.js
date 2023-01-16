64e3808352278dd76baab9aee565b3c794728a52
kjmartens <kelsey@solspace.com>
2023-01-16 11:29:09 -0600

    Updating copyright years

--- src/external/scripts/composer/components/PropertyEditor/PropertyItems/CustomProperty.js
+++ src/external/scripts/composer/components/PropertyEditor/PropertyItems/CustomProperty.js
@@ -3,7 +3,7 @@
  *
  * @package       Solspace:Freeform
  * @author        Solspace, Inc.
- * @copyright     Copyright (c) 2008-2022, Solspace, Inc.
+ * @copyright     Copyright (c) 2008-2023, Solspace, Inc.
  * @link          https://docs.solspace.com/expressionengine/freeform/v3/
  * @license       https://docs.solspace.com/license-agreement/
  */
--- src/freeform_next/ext.freeform_next.php
+++ src/freeform_next/ext.freeform_next.php
@@ -20,7 +20,7 @@ require_once version_compare(PHP_VERSION, '8.0.0') < 0 ? __DIR__ . '/php7/vendor
  *
  * @package       Solspace:Freeform
  * @author        Solspace, Inc.
- * @copyright     Copyright (c) 2008-2022, Solspace, Inc.
+ * @copyright     Copyright (c) 2008-2023, Solspace, Inc.
  * @link          https://docs.solspace.com/expressionengine/freeform/v3/
  * @license       https://docs.solspace.com/license-agreement/
  */
