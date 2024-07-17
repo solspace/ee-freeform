876cb1aa4c0daa209a6d8a6c4bfdf3c93a0416f2
kjmartens <kelsey@solspace.com>
2024-07-17 15:50:54 -0500

    Adjust version

--- src/freeform_next/addon.setup.php
+++ src/freeform_next/addon.setup.php
@@ -27,7 +27,7 @@ if (FreeformHelper::get('version') !== FREEFORM_EXPRESS && !file_exists($cacheDi
     if (!$ftExists) {
         ee()->db->insert('exp_fieldtypes', [
             'name'                => 'freeform_next',
-            'version'             => '3.0.5',
+            'version'             => '3.1.0',
             'settings'            => 'YTowOnt9',
             'has_global_settings' => 'n',
         ]);
