77d64d7fae005562b4e469d0a1ecc5acd3875f9d
Sean Delaney <sean@solspace.com>
2024-10-15 20:31:57 +0100

    fix(SFT-1242): multiple recaptcha instances on same page (#182)

--- src/external/scripts/cp/form-submit.js
+++ src/external/scripts/cp/form-submit.js
@@ -1,13 +1,18 @@
-const form = document.getElementById('{{FORM_ANCHOR}}').parentElement;
+document.addEventListener('DOMContentLoaded', () => {
+  let anchor = document.getElementById('{{FORM_ANCHOR}}');
+  if (anchor) {
+    let form = anchor.parentElement;
+    if (form) {
+      form.addEventListener('submit', () => {
+        const submitButtonList = form.querySelectorAll('[type=submit]:not([name={{PREV_BUTTON_NAME}}])');
+        for (const submit of submitButtonList) {
+          submit.disabled = true;
 
-form.addEventListener('submit', () => {
-  const submitButtonList = form.querySelectorAll('[type=submit]:not([name={{PREV_BUTTON_NAME}}])');
-
-  for (const submit of submitButtonList) {
-    submit.disabled = true;
-
-    setTimeout(function () {
-      submit.disabled = false;
-    }, 600000);
+          setTimeout(function () {
+            submit.disabled = false;
+          }, 600000);
+        }
+      });
+    }
   }
 });
